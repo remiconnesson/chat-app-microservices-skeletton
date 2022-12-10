2b09881453eb2688f5112bb58f3bc453fce990cb
Rémi Connesson <remiconnesson2@gmail.com>
2022-12-10 23:04:49 +0100

    🧦 -- websocket going through traefik

--- chat-websocket/app/main.py
+++ chat-websocket/app/main.py
@@ -18,7 +18,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://localhost/chat/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
