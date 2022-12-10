21b179cc50d76f1914e7737cdbb53900a49320ee
Rémi Connesson <remiconnesson2@gmail.com>
2022-12-10 15:57:35 +0100

    🎨 -- Starter server for the SPA

---
+++ spa/main.go
@@ -0,0 +1,69 @@
+// from https://github.com/gorilla/mux#serving-single-page-applications
+
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"os"
+	"log"
+	"path/filepath"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("serving request...")
+
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/health",
+		func(w http.ResponseWriter, r *http.Request) {
+			json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+		})
+
+	spa := spaHandler{staticPath: "www/dist", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:4444",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	fmt.Println("Listening and serving on port 4444")
+
+	log.Fatal(srv.ListenAndServe())
+	fmt.Println("serving one request...")
+}
