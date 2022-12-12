b5f03d46d4e7c48a8e687e9b1d9f98a994bd91dc
Rémi Connesson <remiconnesson2@gmail.com>
2022-12-12 09:47:37 +0000

    🧦 -- handle several clients

--- chat-websocket/app/main.py
+++ chat-websocket/app/main.py
@@ -8,6 +8,27 @@ assert JWT_SECRET is not None, "Please set JWT_SECRET"
 
 app = FastAPI()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
 async def auth_token(
     websocket: WebSocket,
     session: str | None = Cookie(default=None),
@@ -22,10 +43,13 @@ async def websocket_endpoint(
     websocket: WebSocket,
     session: str = Depends(auth_token)
 ):
-    await websocket.accept()
-    while True:
-        try:
+    client_id = session["id"]
+    await manager.connect(websocket)
+    try:
+        while True:
             data = await websocket.receive_text()
-            await websocket.send_text(f"you are {session} : Message text was: {data}")
-        except WebSocketDisconnect:
-            pass
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
